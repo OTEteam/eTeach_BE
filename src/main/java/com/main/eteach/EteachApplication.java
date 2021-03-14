848df1b487e9392d7bbc81d6e8d03897fa06a462
baopham <baopham101@gmail.com>
2021-03-14 11:33:19 +0700

    initial commit

---
+++ src/main/java/com/main/eteach/EteachApplication.java
@@ -0,0 +1,13 @@
+package com.main.eteach;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EteachApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EteachApplication.class, args);
+    }
+
+}
