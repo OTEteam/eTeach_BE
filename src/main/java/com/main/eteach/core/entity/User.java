cc5ffb5b9edd7617cd6410886c9cb8362dde5987
nguyentrinhan-dev <nguyentrinhan.dev@gmail.com>
2021-04-01 21:46:26 +0700

    authenticated

--- src/main/java/com/main/eteach/core/entity/User.java
+++ src/main/java/com/main/eteach/core/entity/User.java
@@ -7,7 +7,7 @@ import javax.validation.constraints.NotBlank;
 import java.util.Set;
 
 @Entity
-@Table(name = "user", uniqueConstraints = {
+@Table(name = "users", uniqueConstraints = {
         @UniqueConstraint(columnNames = {"username"}),
         @UniqueConstraint(columnNames = {"email"})
 })
